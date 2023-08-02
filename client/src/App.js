831b63988e308f6a91c32922f967d5a5722ae71a
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-08-02 16:15:11 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+// App.js (or index.js)
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import BestBooks from './components/BestBooks';
+import About from './components/About';
+
+const App = () => {
   return (
-    <div className="App">
-     Sarah, Jess & Paolo
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <BestBooks />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
